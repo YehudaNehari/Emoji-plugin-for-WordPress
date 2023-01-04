9ae13bae659ff057f0d16ced46e5955bd4a211d1
YehudaNehari <yehuda.n22@gmail.com>
2023-01-04 16:32:37 +0200

    disabel gutenberg

--- emoji.php
+++ emoji.php
@@ -7,6 +7,8 @@
 * Author URI: https://www.yehuda-nehari.com/
 **/
 
+add_filter("use_block_editor_for_post_type", "disable_gutenberg_editor");
+function disable_gutenberg_editor() { return false; }
 
 // Deduplication smilies
 function fa_get_emojis() {
